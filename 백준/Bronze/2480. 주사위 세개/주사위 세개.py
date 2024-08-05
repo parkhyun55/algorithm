0a820d5a9532e99606a969da57d874d1240682d9
park hyun <104882490+parkhyun55@users.noreply.github.com>
2024-08-05 15:55:17 +0900

    [Bronze IV] Title: 주사위 세개, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.py
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -1,29 +1,17 @@
-a, b, c = input().split()
-a = int(a)
-b = int(b)
-c = int(c)
-money = 0
-
-if a == b and b == c:
-    money = 10000 + a * 1000
+a, b, c = map(int, input().split())
+if a == b and b != c:
+    prise = 1000 + a * 100
+elif b == c and c != a:
+    prise = 1000 + b * 100
+elif c == a and a != b:
+    prise = 1000 + c * 100
+elif a == b and b == c:
+    prise = 10000 + a * 1000
 else:
-    if a != b and b != c and c != a:
-        if a >= b and a >= c:
-            money = a * 100
-        else:
-            if b >= c and b >= a:
-                money = b * 100
-            else:
-                if c >= a and c >= b:
-                    money = c * 100
-    else:
-        if a == b and b != c:
-            money = 1000 + a * 100
-        else:
-            if b == c and c != a:
-                money = 1000 + b * 100
-            else:
-                if c == a and a != b:
-                    money = 1000 + c * 100
-
-print(money)
+    if a > b and a > c:
+        prise = 100 * a
+    elif b > c and b > a:
+        prise = 100 * b
+    elif c > a and c > b:
+        prise = 100 * c
+print(prise)
