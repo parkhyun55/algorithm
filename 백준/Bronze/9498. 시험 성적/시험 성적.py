783f3745bd15d683d1537237fbed9741db9b08f5
park hyun <104882490+parkhyun55@users.noreply.github.com>
2024-08-05 12:49:06 +0900

    [Bronze V] Title: 시험 성적, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.py
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -1,15 +1,11 @@
-a = int(input())
-
-if 90 <= a <= 100:
-    print('A')
-else:
-    if 80 <= a < 90:
-        print('B')
-    else:
-        if 70 <= a < 80:
-            print('C')
-        else:
-            if 60 <= a < 70:
-                print('D')
-            else:
-                print('F')
+score = int(input())
+if score >= 90 :
+    print("A")
+elif score >= 80 :
+    print("B")
+elif score >= 70 :
+    print("C")
+elif score >= 60 :
+    print("D")
+else :
+    print("F")
