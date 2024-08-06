6c7071a8962183da0b5268e2687badf5b35712da
park hyun <104882490+parkhyun55@users.noreply.github.com>
2024-08-06 10:58:10 +0900

    [Bronze V] Title: A+B - 8, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/11022. A＋B － 8/A＋B － 8.py
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.py
@@ -1,5 +1,4 @@
 T = int(input())
-
-for i in range(1, T+1, 1):
-    a, b = map(int, input().split())
-    print('Case #' + str(i) + ':', a, '+', b, '=', a + b)
+for i in range(T):
+    A, B = map(int, input().split())
+    print("Case #" + str(i + 1) + ": " + str(A) + " + " + str(B) + " = " + str(A + B))
