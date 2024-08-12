c7d7dc908a13594b66fdc7301cac2deb5613bd71
park hyun <104882490+parkhyun55@users.noreply.github.com>
2024-08-12 16:16:10 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.py
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.py
@@ -1,12 +1,9 @@
-N, M = list(map(int, input().split()))
-A = []
-
+list = []
+N, M = map(int, input().split())
 for i in range(N):
-    A.append(i + 1)
-
-for _ in range(M):
+    list.append(i+1)
+for k in range(M):
     i, j = map(int, input().split())
-    A = A[0:i-1] + A[i-1:j][::-1] + A[j:]
-
-for i in A:
-    print(i, end = ' ')
+    list = list[:i-1] + list[i-1:j][::-1] + list[j:]
+for i in list:
+    print(i, end=" ")
