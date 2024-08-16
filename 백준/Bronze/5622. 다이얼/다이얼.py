595bfb7218200f0ebc820f35379df19d1787f2fa
park hyun <104882490+parkhyun55@users.noreply.github.com>
2024-08-16 14:40:50 +0900

    [Bronze II] Title: 다이얼, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/5622. 다이얼/다이얼.py
+++ 백준/Bronze/5622. 다이얼/다이얼.py
@@ -1,9 +1,9 @@
-dial = ['ABC', 'DEF', 'GHI', 'JKL', 'MNO', 'PQRS', 'TUV', 'WXYZ']
-s = input()
+word = str(input())
+num = ['ABC', 'DEF', 'GHI', 'JKL', 'MNO', 'PQRS', 'TUV', 'WXYZ']
 time = 0
 
-for i in s:
-    for j in dial:
-        if i in j:
-            time = time + dial.index(j) + 3
+for i in word:
+    for j in range(len(num)):
+        if i in num[j]:
+            time = time + j + 3
 print(time)
