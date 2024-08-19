85d08092b7acb81880e11bc302b3af0353812212
park hyun <104882490+parkhyun55@users.noreply.github.com>
2024-08-19 21:43:00 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.py
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.py
@@ -1,11 +1,7 @@
 N = int(input())
 
-for i in range(N):
-    for j in range(N - i - 1):
-        print(" ", end="")
-    print("*" * (2 * i + 1))
-
-for i in range(N - 1):
-    for j in range(i + 1):
-        print(" ", end="")
-    print("*" * ((2 * N - 2 ) - 2 * i - 1))
+for i in range(1, (2 * N)):
+    if i <= N:
+        print(" " * (N - i) + "*" * (2 * i - 1))
+    else:
+        print(" " * (i - N) + "*" * ((2 * N - i - 1) * 2 + 1))
