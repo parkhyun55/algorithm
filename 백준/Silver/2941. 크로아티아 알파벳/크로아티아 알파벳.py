7ad32e9d39bc018c564dba5682c1f74ba1cab8d6
park hyun <104882490+parkhyun55@users.noreply.github.com>
2024-08-27 21:27:48 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
@@ -1,6 +1,8 @@
-s = input()
-s_list = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
+words = str(input())
+croa = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
+cnt = 0
 
-for i in s_list:
-    s = s.replace(i, '!')
-print(len(s))
+for i in croa:
+    words = words.replace(i, "*")
+
+print(len(words))
