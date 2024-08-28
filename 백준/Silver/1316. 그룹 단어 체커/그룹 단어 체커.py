624a2e25ffd51aa8f96c5ed442a67b67c438702a
park hyun <104882490+parkhyun55@users.noreply.github.com>
2024-08-28 17:22:35 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
@@ -1,29 +1,16 @@
-num = int(input())
-cnt = 0
+N = int(input())
+group_words = []
+cnt = N
 
-for i in range(num):
-    words = input() # i-th 단어 입력
-    bow = []
-    # 만약 문자열이 1개로만 이루어져있다면 그룹단어임.
-    if len(words) == 1:
-        cnt += 1
-    else: 
-        for j in range(len(words)):
-            if j == 0:
-                bow.append(words[0])
-            elif j > 0:
-                if words[j] == words[j-1]:
-                    bow.append(words[j])
-                    if j == len(words)-1:
-                        cnt += 1
-                    
-                elif words[j] != words[j-1]: # j번째 철자가 앞의 철자와 다를 때
-                    if words[j] not in bow:
-                        if j == len(words)-1:
-                            cnt += 1
-                        else:
-                            bow.append(words[j])
-                    elif words[j] in bow: # j번째 철자가 앞에서 등장했다면 중단
-                        break
-                
+for i in range(N):
+    group_words.append(str(input()))
+
+for i in group_words:
+    for j in range(len(i) - 1):
+        if i[j] == i[j+1]:
+            pass
+        elif i[j] in i[j+1:]:
+            cnt = cnt - 1
+            break
+
 print(cnt)
