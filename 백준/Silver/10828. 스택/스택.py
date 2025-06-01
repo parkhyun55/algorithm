e533587dd43fbf8cc80b82328bce5cc626e275a8
park hyun <104882490+parkhyun55@users.noreply.github.com>
2025-06-01 16:57:06 +0900

    [Silver IV] Title: 스택, Time: 36 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.py
+++ 백준/Silver/10828. 스택/스택.py
@@ -1,30 +1,31 @@
 import sys
 
-N = int(sys.stdin.readline())
+N = int(input())
 stack = []
 
 for i in range(N):
-    q = sys.stdin.readline().split()
-    if q[0] == 'push':
-        stack.append(q[1])
-        
-    elif q[0] == 'pop':
-        if len(stack) == 0:
-            print(-1)
-        else:
+    s = sys.stdin.readline().split()
+
+    if s[0] == 'push':
+        stack.append(s[1])
+
+    elif s[0] == 'pop':
+        if stack:
             print(stack.pop())
-    
-    elif q[0] == 'size':
-        print(len(stack))
-    
-    elif q[0] == 'empty':
-        if len(stack) == 0:
-            print("1")
         else:
-            print("0")
-    
-    elif q[0] == 'top':
-        if len(stack) == 0:
             print(-1)
+
+    elif s[0] == 'size':
+        print(len(stack))
+
+    elif s[0] == 'empty':
+        if stack:
+            print(0)
         else:
+            print(1)
+
+    elif s[0] == 'top':
+        if stack:
             print(stack[-1])
+        else:
+            print(-1)
