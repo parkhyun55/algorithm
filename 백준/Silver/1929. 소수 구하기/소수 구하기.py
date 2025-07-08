843aaf19277daa86464be958221a25cf8b091ab3
park hyun <104882490+parkhyun55@users.noreply.github.com>
2025-07-08 14:34:53 +0900

    [Silver III] Title: 소수 구하기, Time: 5904 ms, Memory: 34536 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -0,0 +1,12 @@
+import math
+
+M, N = map(int, input().split())
+for i in range(M, N + 1, 1):
+    if i < 2:
+        continue
+            
+    for j in range(2, int(math.sqrt(i)) + 1):
+        if i % j == 0:
+            break
+    else:
+        print(i)
